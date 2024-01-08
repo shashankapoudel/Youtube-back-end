9cef9803527777a6ff54e5fa7def27204b42582a
shashankapoudel <poudelshashanka205@gmail.com>
2024-01-08 18:44:21 +0545

    connect to mongo atlas

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+
+This is a  series on backend with javascript
+-
---
+++ src/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+/* first approach
+arko approach eslai db ma index.js banayera use garni
+import { Express } from 'express';
+const app = express()
+
+
+   (async () => {
+
+       try {
+           mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+           app.on("error", (error) => {
+               console.log("error");
+               throw error
+           }) //listens error event
+
+           app.listen(process.env.PORT, () => {
+               console.log(`App is listening on port ${process.env.PORT}`);
+           })
+       } catch (error) {
+           console.log("ERROR", error);
+           throw err
+       }
+
+   })()
+   */
